a5ed2a67aa2bc6ba88f513376635e689b6d12cb8
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2022-03-29 12:09:36 +0200

    integrating html and css with express and refining iterations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+
+var dataBike = [
+  {
+    nom: "BIKO45",
+    prix: 679,
+    image: "images/bike-1.jpg",
+  },
+  {
+    nom: "ZOOK7",
+    prix: 799,
+    image: "images/bike-2.jpg",
+  },
+  {
+    nom: "LIKO89",
+    prix: 839,
+    image: "images/bike-3.jpg",
+  },
+  {
+    nom: "GEWO8",
+    prix: 1249,
+    image: "images/bike-4.jpg",
+  },
+  {
+    nom: "KIWIT",
+    prix: 899,
+    image: "images/bike-5.jpg",
+  },
+  {
+    nom: "NASAY",
+    prix: 1399,
+    image: "images/bike-6.jpg",
+  },
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { dataBike });
+});
+
+var dataCardBike = [
+  { quantity: 2, bikeIndex: 0 },
+  { quantity: 1, bikeIndex: 1 },
+  { quantity: 1, bikeIndex: 4 },  
+]
+
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { dataBike, dataCardBike });
 });
 
 module.exports = router;
